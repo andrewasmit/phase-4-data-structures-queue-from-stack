fcc5b0d4662f96431ad7b68d9f233535aed1a5a5
andrew <andrewasmit@hotmail.com>
2023-03-29 20:42:43 -0500

    commit tree 434bcc23268a6b4bdbc3101ff5adea0d5fce5523

--- lib/queue_from_stack.rb
+++ lib/queue_from_stack.rb
@@ -1,3 +1,39 @@
 require_relative './stack'
+require 'pry'
 
-# your code here
+class MyQueue
+
+    def initialize 
+        @s1 = Stack.new
+        @s2 = Stack.new
+    end    
+
+    def add value
+        until @s1.empty? do
+            @s2.push(@s1.pop)
+        end
+        @s2.push value
+        until @s2.empty? do
+            @s1.push(@s2.pop)
+        end
+    end
+
+    def remove
+        # binding.pry
+        # until @s1.empty? do
+        #     @s2.push(@s1.pop)
+        # end
+        # result = @s2.pop
+        # binding.pry
+        # until @s2.empty? do
+        #     @s1.push(@s2.pop)
+        # end
+        # result
+        @s1.pop
+    end
+
+    def peek
+        @s1.peek
+    end
+
+end
